b363d207493ea09abc918ccd834d17aad2348194
Choaib-Routbi <choaibroutbi991990@gmail.com>
2025-10-01 12:31:32 +0100

    Update homepage URL and base path to reflect project name change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/search-countries-flags/',
+  base: '/Countries-and-Flags/',
   plugins: [react()],
 })
